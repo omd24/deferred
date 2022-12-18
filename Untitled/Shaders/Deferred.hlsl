997f15fa37c8eb8042d651b250036077a0a8caba
omd24 <omid.miresmaeili@gmail.com>
2022-12-18 15:44:15 +0100

    #FIX Should hav used uv gradients with anisotropic sampler

--- Untitled/Shaders/Deferred.hlsl
+++ Untitled/Shaders/Deferred.hlsl
@@ -81,7 +81,57 @@ void CS(in uint3 DispatchID : SV_DispatchThreadID, in uint GroupIndex : SV_Group
     Texture2D AlbedoMap = Tex2DTable[NonUniformResourceIndex(matIndices.Albedo)];
     Texture2D NormalMap = Tex2DTable[NonUniformResourceIndex(matIndices.Normal)];
 
-    float4 color = AlbedoMap.SampleLevel(AnisoSampler, uv, 0);
+    float2 uvDX = 0.0f;
+    float2 uvDY = 0.0f;
+
+    // Calculate UV gradiants:
+    {
+    float2 zwGradients = uvMap[pixelPos].zw;
+    // Compute gradients, trying not to walk off the edge of the triangle that isn't coplanar
+    float4 zwGradUp = uvMap[int2(pixelPos) + int2(0, -1)];
+    float4 zwGradDown = uvMap[int2(pixelPos) + int2(0, 1)];
+    float4 zwGradLeft = uvMap[int2(pixelPos) + int2(-1, 0)];
+    float4 zwGradRight = uvMap[int2(pixelPos) + int2(1, 0)];
+
+    uint matIDUp = materialIDMap[int2(pixelPos) + int2(0, -1)] ;
+    uint matIDDown = materialIDMap[int2(pixelPos) + int2(0, 1)];
+    uint matIDLeft = materialIDMap[int2(pixelPos) + int2(-1, 0)];
+    uint matIDRight = materialIDMap[int2(pixelPos) + int2(1, 0)];
+
+    const float zwGradThreshold = 0.0025f;
+    bool up = all(abs(zwGradUp.zw - zwGradients) <= zwGradThreshold) && (matIDUp == packedMaterialID);
+    bool down = all(abs(zwGradDown.zw - zwGradients) <= zwGradThreshold) && (matIDDown == packedMaterialID);
+    bool left = all(abs(zwGradLeft.zw - zwGradients) <= zwGradThreshold) && (matIDLeft == packedMaterialID);
+    bool right = all(abs(zwGradRight.zw - zwGradients) <= zwGradThreshold) && (matIDRight == packedMaterialID);
+
+    if(up)
+        uvDY = uv - zwGradUp.xy * DeferredUVScale;
+    else if(down)
+        uvDY = zwGradDown.xy * DeferredUVScale - uv;
+    if(left)
+        uvDX = uv - zwGradLeft.xy * DeferredUVScale;
+    else if(right)
+        uvDX = zwGradRight.xy * DeferredUVScale - uv;
+    // Check for wrapping around due to frac(), and correct for it.
+    if(uvDX.x > 1.0f)
+        uvDX.x -= 2.0f;
+    else if(uvDX.x < -1.0f)
+        uvDX.x += 2.0f;
+    if(uvDX.y > 1.0f)
+        uvDX.y -= 2.0f;
+    else if(uvDX.y < -1.0f)
+        uvDX.y += 2.0f;
+    if(uvDY.x > 1.0f)
+        uvDY.x -= 2.0f;
+    else if(uvDY.x < -1.0f)
+        uvDY.x += 2.0f;
+    if(uvDY.y > 1.0f)
+        uvDY.y -= 2.0f;
+    else if(uvDY.y < -1.0f)
+        uvDY.y += 2.0f;    
+    }
+
+    float4 color = AlbedoMap.SampleGrad(AnisoSampler, uv, uvDX, uvDY);
     // shadingInput.AlbedoMap = AlbedoMap.SampleGrad(AnisoSampler, uv, uvDX, uvDY);
 
     // Texture2D<float4> AlbedoMapTemp = Tex2DTable[NonUniformResourceIndex(SRVIndices.TangentFrameMapIndex)];

28acac82a5a1c01c56b069283b99c1824645f2cf
omd24 <omid.miresmaeili@gmail.com>
2024-05-26 17:35:13 +0200

    #ADD some todos

---
+++ Untitled/GpuDrivenRenderer.cpp
@@ -0,0 +1,54 @@
+
+
+/*
+
+resources (create/destroy)
+
+mesh/meshlet resources:
+StructuredBuffers:
+   meshlet_data_sb 
+   meshlets_vertex_data_sb
+   meshlets_vertex_pos_sb
+   meshlets_sb
+   meshes_sb
+   mesh_bounds_sb
+   mesh_instances_sb
+
+per frame resources
+assuming k_max_frames = 2
+
+   meshlets_instances_sb[ k_max_frames ];
+   meshlets_index_buffer_sb[k_max_frames];
+   meshlets_visible_instances_sb[k_max_frames];
+
+
+geometry (gbuffer) pass prepare
+Indirect draw commands
+   mesh_task_indirect_count_early_sb[k_max_frames];
+   mesh_task_indirect_early_commands_sb[k_max_frames];
+   mesh_task_indirect_culled_commands_sb[k_max_frames];
+
+   mesh_task_indirect_count_late_sb[k_max_frames];
+   mesh_task_indirect_late_commands_sb[k_max_frames];
+
+   meshlet_instances_indirect_count_sb[k_max_frames];
+
+
+- Indirect dispatch calls
+
+- Generate meshlet list
+
+- Cull visible meshlets
+
+- Write counts
+
+- Generate index buffer
+
+main gbuffer renderpass
+- draw_mesh_task_indirect_count
+
+
+*/
+
+
+
